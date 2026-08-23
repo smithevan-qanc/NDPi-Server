ec4a9ee2aed41172421c6e10eb41c2f026043a5f
smithevan-qanc <fwy25569@gmail.com>
2026-08-23 15:06:36 -0500

    [Stable Release] [3.1.389] NOTES:

--- service/functions.js
+++ service/functions.js
@@ -121,7 +121,7 @@ async function checkForUpdate() {
                 try
                 {
                     const update = JSON.parse(output);
-                    console.log(`Selected Version Type: ${update.versionFrom || 'Not Defined'}`);
+                    console.log(`[ ${path.basename(__filename).split('.')[0]} ] Update Version Type: ${update.versionFrom || 'Not Defined'}`);
                     if (update.update_available)
                     { fs.writeFileSync(path.join(process.env.DATA_NDPI_PATH, 'ndpi_version_update_available'), String(update.update_available), 'utf8'); }
                     if (update.newest_version?.ndpi)
